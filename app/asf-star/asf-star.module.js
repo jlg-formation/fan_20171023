45ef5f6bc6fe1ca7a2e437ce56c26f18f0c202ec
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-24 17:04:46 +0200

    code1.5

--- app/asf-star/asf-star.module.js
+++ app/asf-star/asf-star.module.js
@@ -1,42 +1,37 @@
 angular.module('asf-star', [])
-	.directive('asfStar', function () {
-		return {
-			restrict: 'E',
-			scope: {
-				n: '=?note'
-			},
-			bindToController: true,
-			controller: function AsfStarCtrl($scope, $element, $compile) {
-				'ngInject';
-				console.log('AsfStarCtrl start', this, arguments);
+	.component('asfStar', {
+		bindings: {
+			n: '=?note'
+		},
+		controller: function AsfStarCtrl($scope, $element, $compile) {
+			'ngInject';
+			console.log('AsfStarCtrl start', this, arguments);
 
-				this.update = newNote => {
-					console.log('update ', newNote, this);
-					this.n = newNote;
-				};
+			this.update = newNote => {
+				console.log('update ', newNote, this);
+				this.n = newNote;
+			};
 
-				$scope.$watch('$ctrl.n',  () => {
-					let html = '';
-					let note = Math.floor(+this.n) || 0;
+			$scope.$watch('$ctrl.n', () => {
+				let html = '';
+				let note = Math.floor(+this.n) || 0;
 
-					note = (note > 5) ? 5 : note;
-					note = (note < 0) ? 0 : note;
+				note = (note > 5) ? 5 : note;
+				note = (note < 0) ? 0 : note;
 
-					for (let i = 0; i < note; i++) {
-						html += `
+				for (let i = 0; i < note; i++) {
+					html += `
 						<img ng-click="$ctrl.update(${i + 1})" src="./asf-star/img/yellow_star.png">
 						`;
-					}
-					for (let i = note; i < 5; i++) {
-						html += `
+				}
+				for (let i = note; i < 5; i++) {
+					html += `
 						<img ng-click="$ctrl.update(${i + 1})" src="./asf-star/img/white_star.png">
 						`;
-					}
-					$element.html(html);
-					$compile($element.contents())($scope);
-				});
-			},
-			controllerAs: '$ctrl'
-		};
-	});
+				}
+				$element.html(html);
+				$compile($element.contents())($scope);
+			});
+		}
 
+	});
