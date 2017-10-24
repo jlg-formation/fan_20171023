b2ade46736c71eb1b46f383c466339c4736e7800
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-24 16:32:32 +0200

    note

--- app/asf-star/asf-star.module.js
+++ app/asf-star/asf-star.module.js
@@ -8,19 +8,26 @@ angular.module('asf-star', [])
 			controller: function AsfStarCtrl($scope, $element, $attrs) {
 				'ngInject';
 				console.log('AsfStarCtrl start', this, arguments);
-				let html = '';
-				let note = $scope.n || 3;
-				for (let i = 0; i < note; i++) {
-					html += `
-					<img src="./asf-star/img/yellow_star.png">
-					`;
-				}
-				for (let i = note; i < 5; i++) {
-					html += `
-					<img src="./asf-star/img/white_star.png">
-					`;
-				}
-				$element.html(html);
+
+				$scope.$watch('n', function () {
+					let html = '';
+					let note = Math.floor(+$scope.n) || 0;
+
+					note = (note > 5) ? 5 : note;
+					note = (note < 0) ? 0 : note;
+
+					for (let i = 0; i < note; i++) {
+						html += `
+						<img src="./asf-star/img/yellow_star.png">
+						`;
+					}
+					for (let i = note; i < 5; i++) {
+						html += `
+						<img src="./asf-star/img/white_star.png">
+						`;
+					}
+					$element.html(html);
+				});
 			}
 		};
 	});
