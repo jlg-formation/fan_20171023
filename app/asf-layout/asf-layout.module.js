6fe3e5e75728735bd9296dc8e8022b03b0a08a62
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-24 12:18:06 +0200

    to component

--- app/asf-layout/asf-layout.module.js
+++ app/asf-layout/asf-layout.module.js
@@ -2,26 +2,23 @@
 	'use strict';
 
 	angular.module('asf-layout', [])
-	.directive('asfHeader', function() {
-		return {
-			restrict: 'E',
+		.component('asfHeader', {
 			templateUrl: './asf-layout/tmpl/asf-header.html'
-		}
-	})
-	.directive('asfBody', function() {
-		return {
-			restrict: 'E',
-			templateUrl: './asf-layout/tmpl/asf-body.html'
-		}
-	})
-	.directive('asfFooter', function() {
-		return {
-			restrict: 'E',
-			templateUrl: './asf-layout/tmpl/asf-footer.html'
-		}
-	});
+		})
+		.directive('asfBody', function () {
+			return {
+				restrict: 'E',
+				templateUrl: './asf-layout/tmpl/asf-body.html'
+			}
+		})
+		.directive('asfFooter', function () {
+			return {
+				restrict: 'E',
+				templateUrl: './asf-layout/tmpl/asf-footer.html'
+			}
+		});
 
 
 
-	
+
 })();
