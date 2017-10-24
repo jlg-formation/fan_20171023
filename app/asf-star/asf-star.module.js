d641f6f79c6bd1b3c01c6c29fe9bf725f2e51f32
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-24 16:16:43 +0200

    yellow star

--- app/asf-star/asf-star.module.js
+++ app/asf-star/asf-star.module.js
@@ -5,7 +5,18 @@ angular.module('asf-star', [])
 			controller: function AsfStarCtrl($element) {
 				'ngInject';
 				console.log('AsfStarCtrl start', this, arguments);
-				const html = '* * * * *';
+				let html = '';
+				let note = 3;
+				for (let i = 0; i < note; i++) {
+					html += `
+					<img src="./asf-star/img/yellow_star.png">
+					`;
+				}
+				for (let i = note; i < 5; i++) {
+					html += `
+					<img src="./asf-star/img/white_star.png">
+					`;
+				}
 				$element.html(html);
 			}
 		};
