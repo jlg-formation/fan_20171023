97bafcc872f2aafea3062f56a535603e6e13b47d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-24 17:02:18 +0200

    update

--- app/asf-star/asf-star.module.js
+++ app/asf-star/asf-star.module.js
@@ -5,36 +5,38 @@ angular.module('asf-star', [])
 			scope: {
 				n: '=?note'
 			},
+			bindToController: true,
 			controller: function AsfStarCtrl($scope, $element, $compile) {
 				'ngInject';
 				console.log('AsfStarCtrl start', this, arguments);
 
-				$scope.update = newNote => {
+				this.update = newNote => {
 					console.log('update ', newNote, this);
-					$scope.n = newNote;
+					this.n = newNote;
 				};
 
-				$scope.$watch('n', function () {
+				$scope.$watch('$ctrl.n',  () => {
 					let html = '';
-					let note = Math.floor(+$scope.n) || 0;
+					let note = Math.floor(+this.n) || 0;
 
 					note = (note > 5) ? 5 : note;
 					note = (note < 0) ? 0 : note;
 
 					for (let i = 0; i < note; i++) {
 						html += `
-						<img ng-click="update(${i + 1})" src="./asf-star/img/yellow_star.png">
+						<img ng-click="$ctrl.update(${i + 1})" src="./asf-star/img/yellow_star.png">
 						`;
 					}
 					for (let i = note; i < 5; i++) {
 						html += `
-						<img ng-click="update(${i + 1})" src="./asf-star/img/white_star.png">
+						<img ng-click="$ctrl.update(${i + 1})" src="./asf-star/img/white_star.png">
 						`;
 					}
 					$element.html(html);
 					$compile($element.contents())($scope);
 				});
-			}
+			},
+			controllerAs: '$ctrl'
 		};
 	});
 
