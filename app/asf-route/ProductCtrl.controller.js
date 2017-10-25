da61ce18ea7ed935517505f9b6d596a1310dfa16
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-25 14:06:11 +0200

    first step

---
+++ app/asf-route/ProductCtrl.controller.js
@@ -0,0 +1,6 @@
+export const ProductCtrl = function() {
+    'ngInject';
+    this.start = () => {
+        console.log('Start');
+    };
+};
--- app/asf-route/asf-route.module.js
+++ app/asf-route/asf-route.module.js
@@ -2,6 +2,7 @@ import homeHtml from './tmpl/home.html';
 import productsHtml from './tmpl/products.html';
 import servicesHtml from './tmpl/services.html';
 import contactHtml from './tmpl/contact.html';
+import {ProductCtrl} from './ProductCtrl.controller.js';
 
 angular.module('asf-route', ['ui.router'])
 	.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
@@ -14,7 +15,9 @@ angular.module('asf-route', ['ui.router'])
 		$stateProvider.state({
 			name: 'products',
 			url: '/products',
-			template: productsHtml
+			template: productsHtml,
+			controller: ProductCtrl,
+			controllerAs: '$ctrl'
 		});
 		$stateProvider.state({
 			name: 'services',
