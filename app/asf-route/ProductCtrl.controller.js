ec184dc4c22cee5698f9574b2cabcf2e12d9f1cd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-25 14:24:16 +0200

    step 2

--- app/asf-route/ProductCtrl.controller.js
+++ app/asf-route/ProductCtrl.controller.js
@@ -1,6 +1,19 @@
-export const ProductCtrl = function() {
-    'ngInject';
-    this.start = () => {
-        console.log('Start');
-    };
+export const ProductCtrl = function($http,$q) {
+	'ngInject';
+	this.start = () => {
+		console.log('Start');
+		$http.get('../ws/s1').then(response => {
+            console.log('response', response);
+            return $q.all([
+                $http.get('../ws/s2').then(response => {
+                    console.log('response', response);
+                    return $http.get('../ws/s5');
+                }),
+                $http.get('../ws/s3'),
+                $http.get('../ws/s4'),
+            ]);
+		}).catch(error => {
+			console.error('error', error);
+		});
+	};
 };
