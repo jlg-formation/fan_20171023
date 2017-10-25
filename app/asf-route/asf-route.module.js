fe5ccfd6ba6490799a13fb767681f0c24cee1033
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-25 11:09:29 +0200

    add ui router

--- app/asf-route/asf-route.module.js
+++ app/asf-route/asf-route.module.js
@@ -4,26 +4,26 @@ import servicesHtml from './tmpl/services.html';
 import contactHtml from './tmpl/contact.html';
 
 angular.module('asf-route', ['ui.router'])
-	.config(function($stateProvider){
-        $stateProvider.state({
-            name: 'home',
-            url: '/',
-            template: homeHtml
-        });
-        $stateProvider.state({
-            name: 'products',
-            url: '/products',
-            template: productsHtml
-        });
-        $stateProvider.state({
-            name: 'services',
-            url: '/services',
-            template: servicesHtml
-        });
-        $stateProvider.state({
-            name: 'contact',
-            url: '/contact',
-            template: contactHtml
-        });
-    });
-
+	.config(function($stateProvider, $urlRouterProvider) {
+		$stateProvider.state({
+			name: 'home',
+			url: '/',
+			template: homeHtml
+		});
+		$stateProvider.state({
+			name: 'products',
+			url: '/products',
+			template: productsHtml
+		});
+		$stateProvider.state({
+			name: 'services',
+			url: '/services',
+			template: servicesHtml
+		});
+		$stateProvider.state({
+			name: 'contact',
+			url: '/contact',
+			template: contactHtml
+		});
+		$urlRouterProvider.otherwise('/');
+	});
