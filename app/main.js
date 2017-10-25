604831518f5fed138e865847ddbd737053f5dfeb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-25 11:07:02 +0200

    ui-router

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 import 'angular';
+import '@uirouter/angularjs';
 import './asf-layout/asf-layout.module.js';
 import './asf-star/asf-star.module.js';
+import './asf-route/asf-route.module.js';
 import 'angular/angular-csp.css';
 import './style.scss';
 
-angular.module('main', ['asf-layout','asf-star']);
+angular.module('main', ['asf-layout','asf-star', 'asf-route']);
